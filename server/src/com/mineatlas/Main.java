baf568001db303f5dd0ac2a47850c22b939f9c90
Ben Jeffery <ben.jeffery@Well.ox.ac.uk>
2014-04-14 23:47:35 +0100

    Rename and tidy up java project

--- server/src/com/mineatlas/Main.java
+++ server/src/com/mineatlas/Main.java
@@ -1,4 +1,4 @@
-package com.fragondemand;
+package com.mineatlas;
 
 import amidst.Util;
 import amidst.minecraft.IMinecraftInterface;
@@ -34,34 +34,41 @@ public class Main implements Container {
 
     public void handle(Request request, Response response) {
         try {
-            final int x = Integer.parseInt(request.getParameter("x"));
-            final int y = Integer.parseInt(request.getParameter("y"));
-            final int seed = Integer.parseInt(request.getParameter("seed"));
-            final BiomeType type = BiomeType.valueOf(request.getParameter("type"));
+            System.out.println(request.getPath());
+            if (request.getPath().toString().equals("/data")) {
+                final int x = Integer.parseInt(request.getParameter("x"));
+                final int y = Integer.parseInt(request.getParameter("y"));
+                final int seed = Integer.parseInt(request.getParameter("seed"));
+                final BiomeType type = BiomeType.valueOf(request.getParameter("type"));
 
-            final int[] data = Main.getData(seed, type, x, y, 512);
-            final int len = 512*512;
-            byte[] byte_data = new byte[len];
-            for (int i=0; i < len; i++) {
-                byte_data[i] = (byte) data[i];
+                final int[] data = Main.getData(seed, type, x, y, 512);
+                final int len = 512 * 512;
+                byte[] byte_data = new byte[len];
+                for (int i = 0; i < len; i++) {
+                    byte_data[i] = (byte) data[i];
+                }
+
+                response.setValue("Content-Type", "text/plain");
+                response.setValue("Server", "MineData/1.0");
+                response.setValue("Content-Encoding", "gzip");
+                response.setValue("Cache-Control", "public, max-age=200");
+
+                OutputStream out_stream = response.getOutputStream();
+                GZIPOutputStream zos = new GZIPOutputStream(out_stream);
+                zos.write(byte_data);
+                zos.close();
+                out_stream.close();
+            } else {
+                OutputStream out_stream = response.getOutputStream();
+                out_stream.close();
             }
 
-            response.setValue("Content-Type", "text/plain");
-            response.setValue("Server", "MineData/1.0");
-            response.setValue("Content-Encoding", "gzip");
-            response.setValue("Cache-Control", "public, max-age=200");
-
-            OutputStream out_stream = response.getOutputStream();
-            GZIPOutputStream zos = new GZIPOutputStream(out_stream);
-            zos.write(byte_data);
-            zos.close();
-            out_stream.close();
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] list) throws Exception {
+    public static void main(String[] args) throws Exception {
         Util.setMinecraftDirectory();
         MinecraftVersion version = MinecraftVersion.fromVersionPath(new File("/home/benj/.minecraft/versions/1.7.2/"));
         IMinecraftInterface minecraft = new Minecraft(version.getJarFile()).createInterface();
@@ -70,7 +77,7 @@ public class Main implements Container {
         Container container = new Main();
         Server server = new ContainerServer(container);
         Connection connection = new SocketConnection(server);
-        SocketAddress address = new InetSocketAddress(8000);
+        SocketAddress address = new InetSocketAddress(Integer.parseInt(args[0]));
         connection.connect(address);
     }
 }
