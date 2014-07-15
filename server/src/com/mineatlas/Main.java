d8fe1573d8d33d21e12e01448d9634d08c8119e4
Ben Jeffery <ben.jeffery@gmail.com>
2014-07-15 21:47:39 +0100

    Fix race condition glitches

--- server/src/com/mineatlas/Main.java
+++ server/src/com/mineatlas/Main.java
@@ -27,7 +27,7 @@ public class Main implements Container {
         LargeBiomes
     }
 
-    private static synchronized int[] getData(long seed, BiomeType type, int x, int y, int size) {
+    private static int[] getData(long seed, BiomeType type, int x, int y, int size) {
         MinecraftUtil.createWorld(seed, type.toString().toLowerCase()); //'default 'flat' 'amplified' 'largebiomes'
         return MinecraftUtil.getBiomeData(x * size, y * size, size, size);
     }
@@ -41,11 +41,13 @@ public class Main implements Container {
                 final long seed = Long.parseLong(request.getParameter("seed"));
                 final BiomeType type = BiomeType.valueOf(request.getParameter("type"));
 
-                final int[] data = Main.getData(seed, type, x, y, 512);
                 final int len = 512 * 512;
                 byte[] byte_data = new byte[len];
-                for (int i = 0; i < len; i++) {
-                    byte_data[i] = (byte) data[i];
+                synchronized (this){
+                    final int[] data = Main.getData(seed, type, x, y, 512);
+                    for (int i = 0; i < len; i++) {
+                        byte_data[i] = (byte) data[i];
+                    }
                 }
 
                 response.setValue("Content-Type", "text/plain");
